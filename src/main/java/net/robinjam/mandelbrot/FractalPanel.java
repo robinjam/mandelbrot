aa0c23d371a1b436555851bb54281be074b38028
James Robinson <admin@robinjam.net>
2012-04-19 23:56:26 +0100

    Refactor main window into its own class

--- src/main/java/net/robinjam/mandelbrot/FractalPanel.java
+++ src/main/java/net/robinjam/mandelbrot/FractalPanel.java
@@ -6,9 +6,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.*;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 import net.robinjam.mandelbrot.compute.Job;
 import net.robinjam.mandelbrot.compute.JuliaWorker;
 import net.robinjam.mandelbrot.compute.MandelbrotWorker;
@@ -37,26 +38,6 @@ public class FractalPanel extends JPanel implements ActionListener, SelectionLis
         addMouseListener(select);
         addMouseMotionListener(select);
         this.factory = factory;
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(FractalPanel.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            
-        });
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                startJob();
-            }
-
-        });
     }
     
     public void startJob() {
@@ -68,6 +49,7 @@ public class FractalPanel extends JPanel implements ActionListener, SelectionLis
     
     @Override
     public void paint(Graphics graphics) {
+        if (job == null) startJob();
         Graphics2D g = (Graphics2D) graphics;
         g.drawImage(image, null, this);
         select.paint(g);
@@ -80,62 +62,6 @@ public class FractalPanel extends JPanel implements ActionListener, SelectionLis
         if (job.isDone()) {
             timer.stop();
         }
-    }
-    
-    public static void main(String[] args) {
-        final JFrame window = new JFrame("Fractal explorer");
-
-        final FractalPanel mandelbrotPanel = new FractalPanel(800, 640, 1000, MandelbrotWorker.getFactory());
-        mandelbrotPanel.addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseClicked(MouseEvent me) {
-                Complex c = mandelbrotPanel.viewport.getPixel(me.getX(), me.getY(), mandelbrotPanel.getWidth(), mandelbrotPanel.getHeight());
-                FractalPanel juliaPanel = new FractalPanel(400, 320, 1000, JuliaWorker.getFactory(c));
-                JDialog juliaWindow = new JDialog(window, "Julia Set for point " + c);
-                juliaWindow.add(juliaPanel);
-                juliaWindow.pack();
-                juliaWindow.setLocationRelativeTo(window);
-                juliaWindow.setResizable(false);
-                juliaWindow.setVisible(true);
-            }
-
-            @Override
-            public void mousePressed(MouseEvent me) {
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent me) {
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent me) {
-            }
-
-            @Override
-            public void mouseExited(MouseEvent me) {
-            }
-            
-        });
-        
-        window.setMenuBar(new MainMenu(new MainMenu.Callback() {
-
-            @Override
-            public void maxIterationsChanged(int max_iterations) {
-                mandelbrotPanel.max_iterations = max_iterations;
-                mandelbrotPanel.startJob();
-            }
-            
-        }));
-        
-        window.add(mandelbrotPanel);
-        window.pack();
-        
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        window.setVisible(true);
     }
 
     @Override
---
+++ src/main/java/net/robinjam/mandelbrot/MainWindow.java
@@ -0,0 +1,70 @@
+package net.robinjam.mandelbrot;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import net.robinjam.mandelbrot.compute.JuliaWorker;
+import net.robinjam.mandelbrot.compute.MandelbrotWorker;
+
+/**
+ * The main window for the GUI, and the entry point for the application.
+ *
+ * @author James Robinson
+ */
+public class MainWindow extends JFrame implements MouseListener, MainMenu.Callback {
+    
+    FractalPanel mandelbrotPanel = new FractalPanel(800, 600, 1000, MandelbrotWorker.getFactory());
+
+    MainWindow() {
+        super("Fractal Viewer");
+
+        mandelbrotPanel.addMouseListener(this);
+        add(mandelbrotPanel);
+        pack();
+
+        setMenuBar(new MainMenu(this));
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        Complex c = mandelbrotPanel.viewport.getPixel(me.getX(), me.getY(), mandelbrotPanel.getWidth(), mandelbrotPanel.getHeight());
+        FractalPanel juliaPanel = new FractalPanel(400, 320, 1000, JuliaWorker.getFactory(c));
+        JDialog juliaWindow = new JDialog(this, "Julia Set for point " + c);
+        juliaWindow.add(juliaPanel);
+        juliaWindow.pack();
+        juliaWindow.setLocationRelativeTo(this);
+        juliaWindow.setResizable(false);
+        juliaWindow.setVisible(true);
+    }
+
+
+    @Override
+    public void maxIterationsChanged(int max_iterations) {
+        mandelbrotPanel.max_iterations = max_iterations;
+        mandelbrotPanel.startJob();
+    }
+
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+    
+    // Unused listeners
+    
+    
+    @Override
+    public void mousePressed(MouseEvent me) {}
+
+    @Override
+    public void mouseReleased(MouseEvent me) {}
+
+    @Override
+    public void mouseEntered(MouseEvent me) {}
+
+    @Override
+    public void mouseExited(MouseEvent me) {}
+    
+}
